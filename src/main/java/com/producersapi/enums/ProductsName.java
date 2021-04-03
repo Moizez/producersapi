5345624129a480a451e2ad41c1669c13156496c3
moizez <moizezhenrique@gmail.com>
2021-04-02 21:18:33 -0300

    ajustes nos enums

--- src/main/java/com/producersapi/enums/ProductsName.java
+++ src/main/java/com/producersapi/enums/ProductsName.java
@@ -1,5 +1,5 @@
 package com.producersapi.enums;
 
 public enum ProductsName {
-	Feijão, Milho, Batata, LeiteDeVaca, LeiteDeCabra, Mel, Outro;
+	Feijão, Milho, Batata, LeiteDeVaca, LeiteDeCabra, Mel, Peixe, Camarao, FileDePeixe, Outro;
 }
